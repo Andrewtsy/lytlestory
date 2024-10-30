24303e15fe5ed88907481b4b9e952f83868e9dda
Andrew Tao <andrewtsy5000@gmail.com>
2024-10-30 00:39:20 -0700

    Add js story cell interaction

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World :)")
+const story_cells = document.querySelectorAll('.story-image')
+
+story_cells.forEach((el) => {
+    el.addEventListener('mouseenter', () => {
+        el.style.transform = 'scale(1.25)';
+    });
+    el.addEventListener('mouseleave', () => {
+        el.style.transform = 'scale(1)';
+    });
+});
