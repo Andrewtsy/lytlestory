f7235663b276df89da73c311bc912293087c50fa
Andrew Tao <andrewtsy5000@gmail.com>
2024-11-03 17:10:27 -0800

    frontend query from db

--- backend/src/main/java/com/andrewtao/lytlestory/data/Story.java
+++ backend/src/main/java/com/andrewtao/lytlestory/data/Story.java
@@ -2,7 +2,7 @@ package com.andrewtao.lytlestory.data;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
-// import org.springframework.data.annotation.Id;;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "stories")
@@ -13,14 +13,26 @@ public class Story {
 
     private String title;
     private String author;
+    private String genre;
+    private String file_path;
+
+    // @Column(name = "created_at", nullable = false, updatable = false)
+    // private LocalDateTime createdAt;
 
     public Story() {
     }
 
-    public Story(String title, String author) {
+    public Story(String title, String author, String genre, String file_path) {
         this.title = title;
         this.author = author;
+        this.genre = genre;
+        this.file_path = file_path;
     }
+
+    // @PrePersist
+    // protected void onCreate() {
+    // createdAt = LocalDateTime.now();
+    // }
 
     public Long getId() {
         return id;
@@ -32,5 +44,13 @@ public class Story {
 
     public String getAuthor() {
         return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getFilePath() {
+        return file_path;
     }
 }
--- frontend/scripts/story.js
+++ frontend/scripts/story.js
@@ -1,21 +1,22 @@
 console.log("JavaScript file loaded successfully");
 
-const url = "http://localhost:8080/api/story/1"
-
 async function getStory() {
+    url = "http://localhost:8080/api/story/3"
     try {
         const response = await fetch(url, { method: 'GET' });
         const story = await response.json();
-        console.log(story.title);
 
         const titleDiv = document.getElementById('title');
         const authorDiv = document.getElementById('author');
+        const genreDiv = document.getElementById('genre');
 
         const titleContent = story.title;
         const authorContent = story.author;
+        const genreContent = story.genre;
 
         titleDiv.textContent = titleContent;
         authorDiv.textContent = authorContent;
+        genreDiv.textContent = genreContent;
 
     } catch (error) {
         console.error('error');
