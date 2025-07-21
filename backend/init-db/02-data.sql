c38f87270f4ac07ceec4a2b924c3308d43ac5707
andrew tao <andrewtsy5000@gmail.com>
2025-07-20 21:01:58 -0700

    Fix minor issues

--- backend/init-db/02-data.sql
+++ backend/init-db/02-data.sql
@@ -3,4 +3,4 @@ USE mydb;
 INSERT INTO mydb.stories (title, author, genre, file_name) VALUES
     ('Boule De Suif', 'Guy de Maupassant', 'Realism', 'boule_de_suif'),
     ('Gift of the Magi', 'O. Henry', 'Magic Realism', 'gift_of_the_magi'),
-    ('Legend of Sleepy Hollow', 'Washington Irving', 'Realism', 'boule_de_suif');
+    ('The Legend of Sleepy Hollow', 'Washington Irving', 'Realism', 'boule_de_suif');
